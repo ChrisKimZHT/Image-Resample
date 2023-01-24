e4a46fd45ba50e8be08c2f85d645211e24ef8f1f
ChrisKim <1515748548@qq.com>
2023-01-24 23:06:21 +0800

    基本功能

---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+from PIL import Image
+
+
+def get_filepath(dir_path: str, res_list: list) -> list:
+    for file in os.listdir(dir_path):
+        file_path = os.path.join(dir_path, file)
+        if os.path.isdir(file_path):
+            get_filepath(file_path, res_list)
+        else:
+            res_list.append(file_path)
+    return res_list
+
+
+def resample_img(img_path: str, save_path: str, limit: int = 2400, quality: int = 100) -> None:
+    img = Image.open(img_path)
+    width, height = img.size
+    if width > height and width > limit:
+        img = img.resize((limit, int(limit / width * height)))
+    elif width <= height and height > limit:
+        img = img.resize((int(limit / height * width), limit))
+    path = os.path.split(save_path)[0]
+    if not os.path.exists(path):
+        os.makedirs(path)
+    img.save(save_path, quality=quality)
+
+
+ext_list = [".jpg", ".jpeg", ".png", ".bmp", ".gif", ".tiff", ".webp"]
+
+
+def main() -> None:
+    input_path = input("输入文件夹: ")
+    output_path = input("输出文件夹: ")
+    img_list = get_filepath(input_path, [])
+    for img_path in img_list:
+        path, file_and_ext = os.path.split(img_path)
+        file, ext = os.path.splitext(file_and_ext)
+        if ext.lower() in ext_list:
+            save_path = path.replace(input_path, output_path) + "\\" + file + ext
+            resample_img(img_path, save_path)
+            print(f"[ ] 已处理: {img_path}")
+        else:
+            print(f"[!] 已跳过: {img_path}")
+
+
+if __name__ == '__main__':
+    main()
